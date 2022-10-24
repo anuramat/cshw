64007f81c250404e7209227e31ca0e54959d247b
Arsen Nuramatov <anuramat@pm.me>
2022-10-24 04:13:41 +0300

    some more code

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from math import sin
 def main():
     print('this is a really complex piece of software')
+    for i in range(40):
+        n = int((sin(i/6)+ 1)*60)
+        print('*'*n + ' '*(60-n))
 
 if __name__ == '__main__':
     main()
